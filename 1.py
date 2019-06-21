fcdad1765de422581b1d7abf6261d306eb8769a0
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-06-21 14:16:10 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
-n=int(input())
-if(n>0):
+n1=int(input())
+if(n1>0):
   print("Positive")
-elif(n<0):
-  print("Negative")
+elif(n1<0):
+  print("Negtive")
 else:
-  print("Zero")
+  print("zero")
+
